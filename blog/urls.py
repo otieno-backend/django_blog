1ee752398b3154a18f7279dc6f549280b8debebd
Otibra <otienobrian718@gmail.com>
2026-09-05 23:00:42 -0700

    Add registration login logout and profile views

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.home, name="home"),
     path("login/",auth_views.LoginView.as_view(template_name="blog/login.html"),name="login",),
     path("logout/",auth_views.LogoutView.as_view(),name="logout",),
     path("register/", views.register, name="register"),
